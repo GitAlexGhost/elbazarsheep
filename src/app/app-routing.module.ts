5392fb05fcf75646eb4ab017d6f3dbcd4c485752
MrAlexG <alex.c@hotmail.com>
2020-04-05 12:46:56 -0400

    Portafilio seccion 3 OK

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PortafolioComponent } from './page/portafolio/portafolio.component';
+import { AboutComponent } from './page/about/about.component';
+import { ItemComponent } from './page/item/item.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+{path: 'inicio', component: PortafolioComponent},
+{path: 'about', component: AboutComponent},
+{path: 'item', component: ItemComponent},
+{path: '**', pathMatch: 'full', redirectTo: 'inicio'}
+
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { useHash: true } )],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
